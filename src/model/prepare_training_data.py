3ce694b1b987a78df3beff790fd7e491f0ce1360
wuxxx949 <timnaka123@gmail.com>
2023-03-02 23:43:55 -0500

    make output as df

--- src/model/prepare_training_data.py
+++ src/model/prepare_training_data.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import os
 from datetime import datetime
 from typing import Dict, List, Optional, Tuple
@@ -80,6 +81,7 @@ class ModelDataPrep:
     def __init__(
         self,
         news_path: str,
+        save_dir_path: str,
         training_start_date: datetime,
         validation_start_date: datetime,
         validation_end_date: datetime
@@ -87,15 +89,25 @@ class ModelDataPrep:
         """
         Args:
             news_path (str): path to news directory
+            save_dir_path (str): path to news directory
             training_start_date (datetime): training start date, inclusive
             validation_start_date (datetime): validation start date, inclusive
             validation_end_date (datetime): validation end date, non-inclusive
         """
         self.news_path = news_path
+        self.save_dir_path = save_dir_path
         self.training_start_date = training_start_date
         self.validation_start_date = validation_start_date
         self.validation_end_date = validation_end_date
+        self._load_bert_embeddings()
 
+
+    def _load_bert_embeddings(self):
+        bert_embedding = pd.read_parquet(
+            os.path.join(self.save_dir_path, 'pca_embedding_df.parquet.gzip')
+            )
+        # use date as index for time slicing
+        self.bert_embedding = bert_embedding.reset_index(level=1, drop=False)
 
     def _news_file_filter(
         self,
@@ -123,13 +135,21 @@ class ModelDataPrep:
 
         return out
 
+    @staticmethod
+    def _tfidf_to_df(data_dict: Dict[str, np.array]) -> pd.DataFrame:
+        df = pd.DataFrame(data_dict).T \
+            .reset_index(names='news_id') \
+            .rename(lambda x: f'f{x}' if str(x).isdigit() else x, axis=1)
+
+        return df
+
     def _get_tfidf_vector(
         self,
         tfidf_start_date: datetime,
         tfidf_end_date: datetime,
         start_date: Optional[datetime] = None,
         end_date: Optional[datetime] = None,
-        ) -> Dict[str, np.array]:
+        ) -> pd.DataFrame:
         """calculate tfidf embedding for given date inputs
 
         Args:
@@ -171,10 +191,10 @@ class ModelDataPrep:
             trained_vecterizer=vectorizer,
             news_id=news_id)
 
-        return out
+        return self._tfidf_to_df(out)
 
     @property
-    def training_tfidf_vector(self) -> Dict[str, np.array]:
+    def training_tfidf_df(self) -> pd.DataFrame:
         """tfidf based on training peroid data
         """
         embedding = self._get_tfidf_vector(
@@ -185,7 +205,7 @@ class ModelDataPrep:
         return embedding
 
     @property
-    def validation_tfidf_vector(self) -> Dict[str, np.array]:
+    def validation_tfidf_df(self) -> pd.DataFrame:
         """tfidf embedding based on both training and validation peroid data
         """
         embedding = self._get_tfidf_vector(
@@ -197,6 +217,41 @@ class ModelDataPrep:
 
         return embedding
 
+    def _get_bert_vector(self, start_date: datetime, end_date: datetime) -> pd.DataFrame:
+        start_date = str(start_date.date())
+        end_date = str(end_date.date())
+
+        return self.bert_embedding[start_date: end_date]
+
+    @property
+    def training_bert_df(self):
+        end_date = self.validation_start_date - dt.timedelta(1)
+        out = self._get_bert_vector(
+            start_date=self.training_start_date,
+            end_date=end_date
+            )
+
+        return out
+
+    @property
+    def validation_bert_df(self):
+        end_date = self.validation_end_date - dt.timedelta(1)
+        out = self._get_bert_vector(
+            start_date=self.validation_start_date,
+            end_date=end_date
+            )
+
+        return out
+
+    def prep_model_data(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        training_df = self.training_bert_df \
+            .merge(self.training_tfidf_df, on='news_id', how='inner')
+
+        validation_df = self.validation_bert_df \
+            .merge(self.validation_tfidf_df, on='news_id', how='inner')
+
+        return training_df, validation_df
+
 
 if __name__ == '__main__':
     dm = DateManager(news_path='/home/timnaka123/Documents/financial-news-dataset/ReutersNews106521')
@@ -204,9 +259,22 @@ if __name__ == '__main__':
 
     mdp = ModelDataPrep(
         news_path='/home/timnaka123/Documents/financial-news-dataset/ReutersNews106521',
+        save_dir_path='/home/timnaka123/Documents/stock_embedding_nlp/src/data',
         training_start_date=t_start_date,
         validation_start_date=v_start_date,
         validation_end_date=v_end_date
     )
 
-    train_tfidf_embedding = mdp.training_tfidf_vector
+    train_df, valid_df = mdp.prep_model_data()
+
+    train_tfidf_embedding = mdp.training_tfidf_df
+    train_bert_embedding = mdp.training_bert_df
+
+
+    bert_embedding = pd.read_parquet(
+        '/home/timnaka123/Documents/stock_embedding_nlp/src/data/pca_embedding_df.parquet.gzip'
+        )
+    bert_embedding = bert_embedding.reset_index(level=1, drop=False)
+
+    # datetime index are inclusive on both ends
+    sub_bert = bert_embedding[str(t_start_date.date()): str(v_start_date.date())]
