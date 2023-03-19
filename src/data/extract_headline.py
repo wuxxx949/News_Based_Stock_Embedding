93c43a04e3fe9a2d88e1f2b34d37d126fcd18f4b
wuxxx949 <timnaka123@gmail.com>
2023-03-18 23:12:26 -0400

    bloomberg file headline extraction

--- src/data/extract_headline.py
+++ src/data/extract_headline.py
@@ -9,17 +9,30 @@ from typing import Any, List, Tuple
 from src.data.sp500 import hist_sp500
 from src.data.utils import STOPWORDS
 
-# test path
-file = 'us-aig-idUSTRE62E0GQ20100315'
-folder = '/home/timnaka123/Documents/financial-news-dataset/ReutersNews106521/20100315'
-path = os.path.join(folder, file)
-tickers=hist_sp500['2013/01/31']
 
+punc_pattern = '([' + ''.join(punctuation) + '])'
+
+def process_punc(text: str, rm_punc: bool) -> str:
+    if rm_punc:
+        text = re.sub(r'U\.S\.', 'us', text)
+        text = re.sub(r'U\.K\.', 'uk', text)
+        text = re.sub(punc_pattern, ' ', text)
+    else:
+        # rm '-' as it always appears in the beginning
+        text = re.sub('^-- ', r'', text)
+        # rm line break in the end
+        text = re.sub('\n$', r'', text)
+        # add space betwween punc
+        text = re.sub(punc_pattern, r' \1 ', text)
+        # at most one space
+        text = re.sub(r' +', ' ', text)
+
+    return text
 
 def reuters_single_file_process(
     path: str,
     tickers: List[str],
-    rm_punctuation: bool,
+    rm_punctuation: bool
     ) -> Tuple[List[str], str]:
     """find all matched ticker in a news
 
@@ -49,17 +62,49 @@ def reuters_single_file_process(
     else:
         matched = []
 
-    punc_pattern = '([' + ''.join(punctuation) + '])'
-    if rm_punctuation:
-        headline = re.sub(r'U\.S\.', 'us', headline)
-        headline = re.sub(punc_pattern, ' ', headline)
-    else:
-        # rm '-' as it always appears in the beginning
-        headline = re.sub('^-- ', r'', headline)
-        # rm line break in the end
-        headline = re.sub('\n$', r'', headline)
-        # add space betwween punc
-        headline = re.sub(punc_pattern, r' \1 ', headline)
-        headline = re.sub(r' +', ' ', headline)
+    headline = process_punc(text=headline, rm_punc=rm_punctuation)
 
     return matched, headline
+
+def bloomberg_single_file_process(
+    path: str,
+    tickers: List[str],
+    rm_punctuation: bool
+    ) -> Tuple[List[str], str]:
+    try:
+        with open(path, 'r', encoding="utf8") as f:
+            lines = f.readlines()
+    except Exception:
+        print(f'fail to load file: {path}')
+        return [], '', ''
+
+    headline = lines[0]
+    headline = re.sub('‘|’|“|”', "'", headline)
+
+    news_text = ' '.join(lines)
+    pattern = '\(' + '\)|\('.join(tickers) + '\)'
+    regexp = re.compile(pattern)
+    matched = set(regexp.findall(news_text))
+    if len(matched) > 0:
+        matched =  [re.search(r'\((.*?)\)',s).group(1) for s in matched]
+    else:
+        matched = []
+
+    headline = process_punc(text=headline, rm_punc=rm_punctuation)
+
+    return matched, headline
+
+
+if __name__ == '__main__':
+    # test reuters path
+    file = 'us-aig-idUSTRE62E0GQ20100315'
+    folder = '/home/timnaka123/Documents/financial-news-dataset/ReutersNews106521/20100315'
+    path = os.path.join(folder, file)
+    tickers=hist_sp500['2013/01/31']
+    print(reuters_single_file_process(path ,tickers, False))
+
+    # test bloomberg path
+    bb_folder = '/home/timnaka123/Documents/financial-news-dataset/bloomberg/2013-11-20'
+    file = 'apple-says-smartphone-inventor-patent-claim-falls-shor'
+    path = os.path.join(bb_folder, file)
+    print(bloomberg_single_file_process(path, tickers, False))
