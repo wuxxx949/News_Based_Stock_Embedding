77050a1769f9e34267a52b278f24bb3c5995054f
wuxxx949 <timnaka123@gmail.com>
2023-03-23 09:02:49 -0400

    utils

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,20 +1,23 @@
+import hashlib
 import os
 import pickle
+import uuid
 from typing import Any, List
 
-
-STOPWORDS = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once',
-             'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for',
-             'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is',
-             's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until',
-             'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were',
-             'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above',
-             'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before',
-             'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves',
-             'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now',
-             'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself',
-             'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my',
-             'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than']
+STOPWORDS = [
+    'ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once',
+    'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for',
+    'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is',
+    's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until',
+    'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were',
+    'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above',
+    'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before',
+    'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves',
+    'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now',
+    'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself',
+    'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my',
+    'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than'
+    ]
 
 def pickle_results(dir: str, name: str, obj: Any) -> None:
     """pickle an object
@@ -52,3 +55,23 @@ def get_path(dir_path: str) -> List[str]:
             path_lst.append(os.path.join(dir_path, subdir, path))
 
     return path_lst
+
+
+def longest_line(fpath: str) -> int:
+    """find the max number of tokens in a line
+
+    Args:
+        fpath (str): file path
+
+    Returns:
+        int: max number of tokens
+    """
+    with open(fpath, 'r', encoding="utf8") as f:
+        lines = f.readlines()
+
+    return max([len(e.split(' ')) for e in lines])
+
+
+def create_uuid_from_string(val: str):
+    hex_string = hashlib.md5(val.encode("UTF-8")).hexdigest()
+    return uuid.UUID(hex=hex_string)
