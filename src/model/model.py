ea734b6074a025e93e956e92dd79b38c44aefabb
wuxxx949 <timnaka123@gmail.com>
2023-04-29 17:05:56 -0400

    add log and use sentence embedding

--- src/model/model.py
+++ src/model/model.py
@@ -1,9 +1,11 @@
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
 
 from src.data.stock_data import get_tickers
+from src.logger import setup_logger
 from src.model.prepare_training_data import ModelDataPrep
+
+logger = setup_logger(logger_name='model', log_file='model.log')
+
 
 tickers = get_tickers(
     dir_path='/home/timnaka123/Documents/stock_embedding_nlp/src/data/',
@@ -17,7 +19,7 @@ ticker_vec_layer.adapt(tickers)
 
 # attention
 attention_layer = tf.keras.layers.Attention()
-class NewsAttention1(tf.keras.layers.Layer):
+class NewsAttention(tf.keras.layers.Layer):
     def __init__(self, dtype=tf.float32, **kwargs):
         super().__init__(dtype=dtype, **kwargs)
 
@@ -37,30 +39,28 @@ class NewsAttention1(tf.keras.layers.Layer):
         market_embedding = tf.concat(market_embedding, axis=1)
         ndays = tf.shape(market_embedding)[1]
         embedding_size = tf.shape(market_embedding)[2]
-        # embedding_size = tf.shape(market_embedding)[2]
-        # tf.reshape(market_embedding, [batch_size, ndays, embedding_size])
 
         return tf.reshape(market_embedding, [batch_size, ndays, embedding_size])
 
-# bert embedding as context vector, which is value of attention
-context_embedding = tf.keras.layers.Input(shape=[5, None, 256])
+# sentence embedding as context vector, which is value of attention
+context_embedding = tf.keras.layers.Input(shape=[5, None, 384])
 # tfidf embedding as word vector, which is key of attention
 word_embedding = tf.keras.layers.Input(shape=[5, None, 64])
 
 
-# # ticker to embedding
+# ticker to embedding
 ticker_inputs = tf.keras.layers.Input(shape=[1], dtype=tf.string)
 ticker_input_ids = ticker_vec_layer(ticker_inputs)
 encoder_embedding_layer = tf.keras.layers.Embedding(len(tickers), 64)
 ticker_embeddings = encoder_embedding_layer(ticker_input_ids)
 
 # market layer from attention
-market_layer = NewsAttention1()
+market_layer = NewsAttention()
 market_embeddings = market_layer([ticker_embeddings, context_embedding, word_embedding])
 
 # time series layer
 ts_layer = tf.keras.layers.Bidirectional(
-    tf.keras.layers.GRU(256, return_state=False)
+    tf.keras.layers.GRU(128, return_state=False)
     )
 # h is defined in Equation (4)
 h = ts_layer(market_embeddings)
@@ -68,25 +68,42 @@ h = ts_layer(market_embeddings)
 # MLP for prediction
 output_layer = tf.keras.layers.Dense(1, activation="sigmoid")
 Y_proba = output_layer(h)
-model = tf.keras.Model(inputs=[context_embedding, word_embedding, ticker_inputs], outputs=[Y_proba])
-model.compile(loss="binary_crossentropy", optimizer="nadam",
-              metrics=["accuracy"])
+model = tf.keras.Model(
+    inputs=[context_embedding, word_embedding, ticker_inputs],
+    outputs=[Y_proba]
+    )
+
+model.compile(
+    loss="binary_crossentropy",
+    optimizer="nadam",
+    metrics=["accuracy"]
+    )
 
 
 if __name__ == '__main__':
     from src.model.prepare_training_data import DateManager
 
-    dm = DateManager(news_path='/home/timnaka123/Documents/financial-news-dataset/ReutersNews106521')
+    dm = DateManager(
+        reuters_news_path='/home/timnaka123/Documents/financial-news-dataset/ReutersNews106521',
+        bloomberg_news_path='/home/timnaka123/Documents/financial-news-dataset/bloomberg'
+        )
+
     t_start_date, v_start_date, v_end_date = dm.get_model_date(training_len=3, validation_len=1)
 
     mdp = ModelDataPrep(
-        news_path='/home/timnaka123/Documents/financial-news-dataset/ReutersNews106521',
+        reuters_news_path='/home/timnaka123/Documents/financial-news-dataset/ReutersNews106521',
+        bloomberg_news_path='/home/timnaka123/Documents/financial-news-dataset/bloomberg',
         save_dir_path='/home/timnaka123/Documents/stock_embedding_nlp/src/data',
         training_start_date=t_start_date,
         validation_start_date=v_start_date,
         validation_end_date=v_end_date
     )
 
-    training_ds, validation_ds = mdp.create_dataset()
-
+    training_ds, validation_ds = mdp.create_dataset(split_method='random')
     hisotry = model.fit(training_ds, validation_data=validation_ds, epochs=10)
+
+    # log model history
+    logger.info(f"training loss: {hisotry.history['loss']}")
+    logger.info(f"validatoin loss: {hisotry.history['val_loss']}")
+    logger.info(f"training accuracy: {hisotry.history['accuracy']}")
+    logger.info(f"validatoin accuracy: {hisotry.history['val_accuracy']}")
