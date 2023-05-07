6065f76cc45efe9f6ff7d1fc65b3c0c0f69e306a
wuxxx949 <timnaka123@gmail.com>
2023-05-07 09:49:17 -0400

    implement optional temporal attention

--- src/model/model.py
+++ src/model/model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam, Nadam
 from tensorflow.keras.optimizers.schedules import CosineDecay
 
 from src.data.stock_data import get_tickers
@@ -26,18 +26,18 @@ class NewsAttention(tf.keras.layers.Layer):
     def __init__(self, dtype=tf.float32, **kwargs):
         super().__init__(dtype=dtype, **kwargs)
 
-    def call(self, raw_inputs):
-        ticker_input = raw_inputs[0]
-        context_input = raw_inputs[1]
-        word_input = raw_inputs[2]
+    def call(self, embedding_inputs):
+        ticker_embedding = embedding_inputs[0]
+        context_embedding = embedding_inputs[1]
+        tfidf_embedding = embedding_inputs[2]
 
         market_embedding = []
-        batch_size = tf.shape(ticker_input)[0]
+        batch_size = tf.shape(ticker_embedding)[0]
 
         for i in range(5):
-            tmp_key = word_input[:, i, :, :]
-            tmp_value = context_input[:, i, :, :]
-            market_embedding.append(attention_layer([ticker_input, tmp_value, tmp_key]))
+            tmp_key = tfidf_embedding[:, i, :, :]
+            tmp_value = context_embedding[:, i, :, :]
+            market_embedding.append(attention_layer([ticker_embedding, tmp_value, tmp_key]))
 
         market_embedding = tf.concat(market_embedding, axis=1)
         ndays = tf.shape(market_embedding)[1]
@@ -50,7 +50,6 @@ context_embedding = tf.keras.layers.Input(shape=[5, None, 384])
 # tfidf embedding as word vector, which is key of attention
 word_embedding = tf.keras.layers.Input(shape=[5, None, 64])
 
-
 # ticker to embedding
 ticker_inputs = tf.keras.layers.Input(shape=[1], dtype=tf.string)
 ticker_input_ids = ticker_vec_layer(ticker_inputs)
@@ -61,16 +60,24 @@ ticker_embeddings = encoder_embedding_layer(ticker_input_ids)
 market_layer = NewsAttention()
 market_embeddings = market_layer([ticker_embeddings, context_embedding, word_embedding])
 
-# time series layer
+# time series layer with optional temporal attention
 ts_layer = tf.keras.layers.Bidirectional(
-    tf.keras.layers.GRU(128, return_state=False)
+    tf.keras.layers.GRU(64, return_sequences=True) # return sequences as h_tau
     )
 # h is defined in Equation (4)
-h = ts_layer(market_embeddings)
+htau = ts_layer(market_embeddings)
+# use embedding vector as v
+v_layer = tf.keras.layers.Embedding(htau.shape[1], htau.shape[2])
+v_vec = v_layer(tf.constant(range(htau.shape[1]), dtype=tf.int8))
+# v_{tau-t} * h_{tau}^O terms
+vh = [tf.matmul(htau[:, i, :], tf.transpose(tf.expand_dims(v_vec[i], axis=0))) for i in range(5)]
+tensors_stacked = tf.stack(vh, axis=1) # shape=(None, 5, 1)
+beta_tau = tf.nn.softmax(tensors_stacked, axis=1) # shape=(None, 5, 1)
+ho= tf.reduce_sum(htau * beta_tau, axis=1)  # shape: (None, 256)
 
 # MLP for prediction
 output_layer = tf.keras.layers.Dense(1, activation="sigmoid")
-Y_proba = output_layer(h)
+Y_proba = output_layer(ho)
 
 model = tf.keras.Model(
     inputs=[context_embedding, word_embedding, ticker_inputs],
@@ -78,15 +85,16 @@ model = tf.keras.Model(
     )
 
 # Adam optimizer with cosine annealing
-initial_learning_rate = 0.002
-lr_schedule = CosineDecay(initial_learning_rate, 2000)
-optimizer = Adam(learning_rate=lr_schedule)
-early_stop = EarlyStopping(monitor='val_loss', patience=1)
+# initial_learning_rate = 0.005
+# lr_schedule = CosineDecay(initial_learning_rate, 2000)
+# optimizer = Adam(learning_rate=lr_schedule)
+
+early_stop = EarlyStopping(monitor='val_accuracy', patience=2)
 
 model.compile(
     loss='binary_crossentropy',
-    # optimizer="nadam",
-    optimizer=optimizer,
+    optimizer=tf.keras.optimizers.Nadam(learning_rate=0.0002),
+    # optimizer=optimizer,
     metrics=['accuracy']
     )
 
@@ -110,11 +118,11 @@ if __name__ == '__main__':
         validation_end_date=v_end_date
     )
 
-    training_ds, validation_ds = mdp.create_dataset(split_method='random', seed_value=88)
+    training_ds, validation_ds = mdp.create_dataset(split_method='random', seed_value=60)
     hisotry = model.fit(
         training_ds,
         validation_data=validation_ds,
-        epochs=10,
+        epochs=20,
         callbacks=[early_stop]
         )
 
