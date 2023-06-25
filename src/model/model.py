a350458d33180c2cea1b02f418bee690bcfbd923
wuxxx949 <timnaka123@gmail.com>
2023-06-24 22:56:50 -0400

    fix ticker length and extract embeddings

--- src/model/model.py
+++ src/model/model.py
@@ -16,7 +16,7 @@ tickers = get_tickers(
 )
 
 ticker_vec_layer = tf.keras.layers.TextVectorization(
-    len(tickers), output_sequence_length=1
+    len(tickers) + 2, output_sequence_length=1
     )
 ticker_vec_layer.adapt(tickers)
 
@@ -53,7 +53,7 @@ word_embedding = tf.keras.layers.Input(shape=[5, None, 64])
 # ticker to embedding
 ticker_inputs = tf.keras.layers.Input(shape=[1], dtype=tf.string)
 ticker_input_ids = ticker_vec_layer(ticker_inputs)
-encoder_embedding_layer = tf.keras.layers.Embedding(len(tickers), 64)
+encoder_embedding_layer = tf.keras.layers.Embedding(len(tickers) + 2, 64)
 ticker_embeddings = encoder_embedding_layer(ticker_input_ids)
 
 # market layer from attention
@@ -85,15 +85,15 @@ model = tf.keras.Model(
     )
 
 # Adam optimizer with cosine annealing
-# initial_learning_rate = 0.005
-# lr_schedule = CosineDecay(initial_learning_rate, 2000)
+# initial_learning_rate = 0.01
+# lr_schedule = CosineDecay(initial_learning_rate, decay_steps=2000, alpha=0.001)
 # optimizer = Adam(learning_rate=lr_schedule)
 
 early_stop = EarlyStopping(monitor='val_accuracy', patience=2)
 
 model.compile(
     loss='binary_crossentropy',
-    optimizer=tf.keras.optimizers.Nadam(learning_rate=0.001),
+    optimizer= tf.keras.optimizers.Nadam(learning_rate=0.0005),
     # optimizer=optimizer,
     metrics=['accuracy']
     )
@@ -101,6 +101,8 @@ model.compile(
 
 if __name__ == '__main__':
     from src.model.prepare_training_data import DateManager
+    from src.data.utils import pickle_results
+
 
     dm = DateManager(
         reuters_news_path='/home/timnaka123/Documents/financial-news-dataset/ReutersNews106521',
@@ -117,7 +119,7 @@ if __name__ == '__main__':
         max_date=end_date
     )
 
-    training_ds, validation_ds = mdp.create_dataset(seed_value=41)
+    training_ds, validation_ds = mdp.create_dataset(seed_value=41, batch_size=64)
     hisotry = model.fit(
         training_ds,
         validation_data=validation_ds,
@@ -130,3 +132,16 @@ if __name__ == '__main__':
     logger.info(f"validatoin loss: {hisotry.history['val_loss']}")
     logger.info(f"training accuracy: {hisotry.history['accuracy']}")
     logger.info(f"validatoin accuracy: {hisotry.history['val_accuracy']}")
+
+    # extract embeddings
+    # TODO: make a function
+    out = {}
+    for t in tickers:
+        out[t] = encoder_embedding_layer(ticker_vec_layer([t])[0])[0].numpy()
+
+    pickle_results(
+        dir='/home/timnaka123/Documents/stock_embedding_nlp/src/model/', 
+        name='embedding.pickle', 
+        obj=out
+        )
+
