d4e385be968b13ae5600bc139974b9f9ade16d20
wuxxx949 <timnaka123@gmail.com>
2023-07-02 17:13:03 -0400

    refactor model script

--- src/model/model.py
+++ src/model/model.py
@@ -1,34 +1,28 @@
 import os
+from typing import List, Tuple
 
 import pandas as pd
 import tensorflow as tf
+from keras.engine.functional import Functional
+from keras.layers.core.embedding import Embedding
+from keras.layers.preprocessing.text_vectorization import TextVectorization
 from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.optimizers import Adam, Nadam
-from tensorflow.keras.optimizers.schedules import CosineDecay
 
-from src.data.stock_data import get_tickers
 from src.logger import setup_logger
 from src.model.prepare_training_data import ModelDataPrep
 
 logger = setup_logger(logger_name='model', log_file='model.log')
 
 
-tickers = pd.read_parquet(
-    os.path.join('/home/timnaka123/Documents/stock_embedding_nlp/src/data/', 'target_df.parquet.gzip')
-    )['ticker'].unique()
-
-ticker_vec_layer = tf.keras.layers.TextVectorization(
-    len(tickers) + 2, output_sequence_length=1
-    )
-ticker_vec_layer.adapt(tickers)
-
 # attention
-attention_layer = tf.keras.layers.Attention()
 class NewsAttention(tf.keras.layers.Layer):
+    """News attention layer described in Eq (3)
+    """
     def __init__(self, dtype=tf.float32, **kwargs):
         super().__init__(dtype=dtype, **kwargs)
 
     def call(self, embedding_inputs):
+        attention_layer = tf.keras.layers.Attention()
         ticker_embedding = embedding_inputs[0]
         context_embedding = embedding_inputs[1]
         tfidf_embedding = embedding_inputs[2]
@@ -47,64 +41,82 @@ class NewsAttention(tf.keras.layers.Layer):
 
         return tf.reshape(market_embedding, [batch_size, ndays, embedding_size])
 
-# sentence embedding as context vector, which is value of attention
-context_embedding = tf.keras.layers.Input(shape=[5, None, 384])
-# tfidf embedding as word vector, which is key of attention
-word_embedding = tf.keras.layers.Input(shape=[5, None, 64])
 
-# ticker to embedding
-ticker_inputs = tf.keras.layers.Input(shape=[1], dtype=tf.string)
-ticker_input_ids = ticker_vec_layer(ticker_inputs)
-encoder_embedding_layer = tf.keras.layers.Embedding(len(tickers) + 2, 64)
-ticker_embeddings = encoder_embedding_layer(ticker_input_ids)
+def get_model(
+    tickers: List[str],
+    learning_rate: float = 0.0005
+    ) -> Tuple[Functional, TextVectorization, Embedding]:
+    """model architecture
 
-# market layer from attention
-market_layer = NewsAttention()
-market_embeddings = market_layer([ticker_embeddings, context_embedding, word_embedding])
+    Args:
+        tickers (List[str]): unique tickers
+        learning_rate (float, optional): learning rate for Nadam. Defaults to 0.0005.
 
-# time series layer with optional temporal attention
-ts_layer = tf.keras.layers.Bidirectional(
-    tf.keras.layers.GRU(64, return_sequences=True) # return sequences as h_tau
-    )
-# h is defined in Equation (4)
-htau = ts_layer(market_embeddings)
-# use embedding vector as v
-v_layer = tf.keras.layers.Embedding(htau.shape[1], htau.shape[2])
-v_vec = v_layer(tf.constant(range(htau.shape[1]), dtype=tf.int8))
-# v_{tau-t} * h_{tau}^O terms
-vh = [tf.matmul(htau[:, i, :], tf.transpose(tf.expand_dims(v_vec[i], axis=0))) for i in range(5)]
-tensors_stacked = tf.stack(vh, axis=1) # shape=(None, 5, 1)
-beta_tau = tf.nn.softmax(tensors_stacked, axis=1) # shape=(None, 5, 1)
-ho= tf.reduce_sum(htau * beta_tau, axis=1)  # shape: (None, 256)
+    Returns:
+        Functional: model object
+    """
+    # map ticker string to int, +2 for reserved int
+    ticker_vec_layer = tf.keras.layers.TextVectorization(
+        len(tickers) + 2, output_sequence_length=1
+        )
+    ticker_vec_layer.adapt(tickers)
 
-# MLP for prediction
-output_layer = tf.keras.layers.Dense(1, activation="sigmoid")
-Y_proba = output_layer(ho)
+    # sentence embedding as context vector, which is value of attention
+    context_embedding = tf.keras.layers.Input(shape=[5, None, 384])
+    # tfidf embedding as word vector, which is key of attention
+    word_embedding = tf.keras.layers.Input(shape=[5, None, 64])
 
-model = tf.keras.Model(
-    inputs=[context_embedding, word_embedding, ticker_inputs],
-    outputs=[Y_proba]
-    )
+    # ticker to embedding
+    ticker_inputs = tf.keras.layers.Input(shape=[1], dtype=tf.string)
+    ticker_input_ids = ticker_vec_layer(ticker_inputs)
+    encoder_embedding_layer = tf.keras.layers.Embedding(len(tickers) + 2, 64)
+    ticker_embeddings = encoder_embedding_layer(ticker_input_ids)
 
-# Adam optimizer with cosine annealing
-# initial_learning_rate = 0.01
-# lr_schedule = CosineDecay(initial_learning_rate, decay_steps=2000, alpha=0.001)
-# optimizer = Adam(learning_rate=lr_schedule)
+    # market layer from attention
+    market_layer = NewsAttention()
+    market_embeddings = market_layer([ticker_embeddings, context_embedding, word_embedding])
 
-early_stop = EarlyStopping(monitor='val_accuracy', patience=2)
+    # time series layer with optional temporal attention
+    ts_layer = tf.keras.layers.Bidirectional(
+        tf.keras.layers.GRU(64, return_sequences=True) # return sequences as h_tau
+        )
+    # h is defined in Equation (4)
+    htau = ts_layer(market_embeddings)
+    # use embedding vector as v
+    v_layer = tf.keras.layers.Embedding(htau.shape[1], htau.shape[2])
+    v_vec = v_layer(tf.constant(range(htau.shape[1]), dtype=tf.int8))
+    # v_{tau-t} * h_{tau}^O terms
+    vh = [tf.matmul(htau[:, i, :], tf.transpose(tf.expand_dims(v_vec[i], axis=0))) for i in range(5)]
+    tensors_stacked = tf.stack(vh, axis=1) # shape=(None, 5, 1)
+    beta_tau = tf.nn.softmax(tensors_stacked, axis=1) # shape=(None, 5, 1)
+    ho= tf.reduce_sum(htau * beta_tau, axis=1)  # shape: (None, 256)
 
-model.compile(
-    loss='binary_crossentropy',
-    optimizer= tf.keras.optimizers.Nadam(learning_rate=0.0005),
-    # optimizer=optimizer,
-    metrics=['accuracy']
-    )
+    # MLP for prediction
+    output_layer = tf.keras.layers.Dense(1, activation="sigmoid")
+    Y_proba = output_layer(ho)
+
+    model = tf.keras.Model(
+        inputs=[context_embedding, word_embedding, ticker_inputs],
+        outputs=[Y_proba]
+        )
+
+    model.compile(
+        loss='binary_crossentropy',
+        optimizer= tf.keras.optimizers.Nadam(learning_rate=learning_rate),
+        metrics=['accuracy']
+        )
+
+    return model, ticker_vec_layer, encoder_embedding_layer
 
 
 if __name__ == '__main__':
     from src.data.utils import pickle_results
     from src.model.prepare_training_data import DateManager
+    tickers = pd.read_parquet(
+        os.path.join('/home/timnaka123/Documents/stock_embedding_nlp/src/data/', 'target_df.parquet.gzip')
+        )['ticker'].unique()
 
+    model, ticker_vec, ticker_embedding = get_model(tickers=tickers)
 
     dm = DateManager(
         reuters_news_path='/home/timnaka123/Documents/financial-news-dataset/ReutersNews106521',
@@ -120,12 +132,13 @@ if __name__ == '__main__':
         min_date=start_date,
         max_date=end_date
     )
-
     training_ds, validation_ds = mdp.create_dataset(seed_value=41, batch_size=64)
+    early_stop = EarlyStopping(monitor='val_accuracy', patience=2)
+
     hisotry = model.fit(
         training_ds,
         validation_data=validation_ds,
-        epochs=20,
+        epochs=1,
         callbacks=[early_stop]
         )
 
@@ -139,11 +152,12 @@ if __name__ == '__main__':
     # TODO: make a function
     out = {}
     for t in tickers:
-        out[t] = encoder_embedding_layer(ticker_vec_layer([t])[0])[0].numpy()
+        out[t] = ticker_embedding(ticker_vec([t])[0])[0].numpy()
 
     pickle_results(
         dir='/home/timnaka123/Documents/stock_embedding_nlp/src/model/',
         name='embedding.pickle',
         obj=out
         )
+    tf.keras.backend.clear_session()
 
