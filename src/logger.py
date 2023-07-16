d3f4285d16f376db1886bcb9463d0aea03f6fb42
wuxxx949 <timnaka123@gmail.com>
2023-07-16 11:54:35 -0400

    specify log dir

--- src/logger.py
+++ src/logger.py
@@ -1,20 +1,28 @@
+import os
 import logging
+
+from src.meta_data import get_meta_data
 
 def setup_logger(
     logger_name: str,
     log_file: str,
-    level=logging.INFO
+    level=logging.INFO,
+    rm_existing_log: bool = False
     ) -> logging.Logger:
     """setup logger
     """
-    logger = logging.getLogger(logger_name)
+    log_dir = get_meta_data()['LOG_DIR']
+    if rm_existing_log:
+        os.rmdir(log_dir)
 
+    logger = logging.getLogger(logger_name)
     logger.setLevel(level)
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(level)
     formatter = logging.Formatter('%(levelname)-8s %(message)s')
     stream_handler.setFormatter(formatter)
 
+    log_file = os.path.join(log_dir, log_file)
     file_handler = logging.FileHandler(log_file, 'a+')
     formatter_with_date = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)-8s %(message)s',
